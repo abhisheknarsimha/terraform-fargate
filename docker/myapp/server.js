9e39cf1b3b25525dd652569535c403471189605b
abhisheknarsimha <41733171+abhisheknarsimha@users.noreply.github.com>
2018-09-12 16:59:53 +0530

    tes

--- docker/myapp/server.js
+++ docker/myapp/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Fargate!');
+  res.send('Hello DFS!');
 });
 
 var server = app.listen(3000, function () {
